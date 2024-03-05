fbba63a99d74ad3cc8b8ad1461a26840ad9a6f0b
cristina-ropot <103998208+cristina-ropot@users.noreply.github.com>
2024-03-05 21:52:18 +0100

    Update resnet.py
    added base model resnet50

--- keras/applications/resnet.py
+++ keras/applications/resnet.py
@@ -317,6 +317,43 @@ def residual_block_v1(
         x= FlipoutDense(64, kl_weight, activation='relu')(x)
         x = layers.Add(name=name + "_add")([shortcut, x])
         x = layers.Activation("relu", name=name + "_out")(x)
+    if(type="base"):
+                if backend.image_data_format() == "channels_last":
+            bn_axis = 3
+        else:
+            bn_axis = 1
+    
+        if conv_shortcut:
+            shortcut = layers.Conv2D(
+                4 * filters, 1, strides=stride, name=name + "_0_conv"
+            )(x)
+            shortcut = layers.BatchNormalization(
+                axis=bn_axis, epsilon=1.001e-5, name=name + "_0_bn"
+            )(shortcut)
+        else:
+            shortcut = x
+    
+        x = layers.Conv2D(filters, 1, strides=stride, name=name + "_1_conv")(x)
+        x = layers.BatchNormalization(
+            axis=bn_axis, epsilon=1.001e-5, name=name + "_1_bn"
+        )(x)
+        x = layers.Activation("relu", name=name + "_1_relu")(x)
+    
+        x = layers.Conv2D(
+            filters, kernel_size, padding="SAME", name=name + "_2_conv"
+        )(x)
+        x = layers.BatchNormalization(
+            axis=bn_axis, epsilon=1.001e-5, name=name + "_2_bn"
+        )(x)
+        x = layers.Activation("relu", name=name + "_2_relu")(x)
+    
+        x = layers.Conv2D(4 * filters, 1, name=name + "_3_conv")(x)
+        x = layers.BatchNormalization(
+            axis=bn_axis, epsilon=1.001e-5, name=name + "_3_bn"
+        )(x)
+    
+        x = layers.Add(name=name + "_add")([shortcut, x])
+        x = layers.Activation("relu", name=name + "_out")(x)
     return x
 
 
